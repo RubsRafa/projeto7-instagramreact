040c5cdffeec990f7f1696af803537a1d0c6814c
RubsRafa <rubiarafaela2000@gmail.com>
2022-11-18 13:41:55 -0300

    Utilizando map e atributos de teste

--- src/componentes/Posts.js
+++ src/componentes/Posts.js
@@ -1,77 +1,50 @@
-export default function Posts () {
-    return (
-        <div class="posts">
-          <div class="post">
-            <div class="topo">
-              <div class="usuario">
-                <img src="assets/img/meowed.svg" />
-                meowed
-              </div>
-              <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
-              </div>
-            </div>
+export default function Posts() {
+  const posts = [{ img: "assets/img/meowed.svg", nome: 'meowed', conteudo: "assets/img/gato-telefone.svg", curtidas: "assets/img/respondeai.svg", curtido: 'respondeai', quantidade:'101.523' },
+  { img: "assets/img/barked.svg", nome: 'barked', conteudo: "assets/img/dog.svg", curtidas: "assets/img/adorable_animals.svg", curtido: 'adorable_animals', quantidade:'99.159' }]
 
-            <div class="conteudo">
-              <img src="assets/img/gato-telefone.svg" />
-            </div>
+  return (
+    <div class="posts">
+      {posts.map ((p) => <Post img={p.img} nome={p.nome} conteudo={p.conteudo} curtidas={p.curtidas} curtido={p.curtido} quantidade={p.quantidade} />)}
+    </div>
+  )
+}
 
-            <div class="fundo">
-              <div class="acoes">
-                <div>
-                  <ion-icon name="heart-outline"></ion-icon>
-                  <ion-icon name="chatbubble-outline"></ion-icon>
-                  <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <div>
-                  <ion-icon name="bookmark-outline"></ion-icon>
-                </div>
-              </div>
+function Post(props) {
+  return (
+    <div data-test="post" class="post">
+      <div class="topo">
+        <div class="usuario">
+          <img src={props.img} />
+          {props.nome}
+        </div>
+        <div class="acoes">
+          <ion-icon name="ellipsis-horizontal"></ion-icon>
+        </div>
+      </div>
 
-              <div class="curtidas">
-                <img src="assets/img/respondeai.svg" />
-                <div class="texto">
-                  Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-                </div>
-              </div>
-            </div>
+      <div class="conteudo">
+        <img data-test="post-image" src={props.conteudo} />
+      </div>
+
+      <div class="fundo">
+        <div class="acoes">
+          <div>
+            <ion-icon data-test="like-post" name="heart-outline"></ion-icon>
+            <ion-icon name="chatbubble-outline"></ion-icon>
+            <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
-
-          <div class="post">
-            <div class="topo">
-              <div class="usuario">
-                <img src="assets/img/barked.svg" />
-                barked
-              </div>
-              <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
-              </div>
-            </div>
-
-            <div class="conteudo">
-              <img src="assets/img/dog.svg" />
-            </div>
-
-            <div class="fundo">
-              <div class="acoes">
-                <div>
-                  <ion-icon name="heart-outline"></ion-icon>
-                  <ion-icon name="chatbubble-outline"></ion-icon>
-                  <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <div>
-                  <ion-icon name="bookmark-outline"></ion-icon>
-                </div>
-              </div>
-
-              <div class="curtidas">
-                <img src="assets/img/adorable_animals.svg" />
-                <div class="texto">
-                  Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                </div>
-              </div>
-            </div>
+          <div>
+            <ion-icon data-test="save-post" name="bookmark-outline"></ion-icon>
           </div>
         </div>
-    )
+
+        <div class="curtidas">
+          <img src={props.curtidas} />
+          <div class="texto">
+          Curtido por <strong>{props.curtido}</strong> e <strong data-test="likes-number">outras {props.quantidade} pessoas</strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/componentes/Usuario.js
+++ src/componentes/Usuario.js
@@ -1,16 +1,24 @@
-let nome = prompt ('Qual é o seu nome?')
 
-export default function Usuario () {
-        return (
-            <div class="usuario">
-              <img src="assets/img/catanacomics.svg" />
-              <div class="texto">
-                <strong>catanacomics</strong>
-                <span>
-                  {nome}
-                  <ion-icon name="pencil"></ion-icon>
-                </span>
-              </div>
-            </div>
-        )    
-    }
+export default function Usuario() {
+
+  //const usuarios = [{ url: "assets/img/catanacomics.svg", nomeFantasia: 'catanacomics', nomeUsuario: 'Catana' }];
+
+  return (
+    <Unico />
+  )
+}
+
+function Unico(props) {
+  return (
+    <div class="usuario" data-test="user">
+      <img data-test="profile-image" src="assets/img/catanacomics.svg" />
+      <div class="texto">
+        <strong>catanacomics</strong>
+        <span>
+        <h1 data-test="name">Catana</h1>
+          <ion-icon data-test="edit-name" name="pencil"></ion-icon>
+        </span>
+      </div>
+    </div>
+  )
+}
